7e8027abdcd981dcf24c42cb800239ebf9e9bb2a
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-15 22:06:33 -0500

    Update umbrella header

--- Sources/CZephyr/include/CZephyr.h
+++ Sources/CZephyr/include/CZephyr.h
@@ -11,4 +11,15 @@
 #include <zephyr/cache.h>
 #include <zephyr/fatal.h>
 #include <zephyr/init.h>
+
+// Bluetooth
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/addr.h>
+#include <zephyr/bluetooth/att.h>
+#include <zephyr/bluetooth/buf.h>
+#include <zephyr/bluetooth/gap.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/l2cap.h>
+#include <zephyr/bluetooth/crypto.h>
