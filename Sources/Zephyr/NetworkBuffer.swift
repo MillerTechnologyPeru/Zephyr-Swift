ac5def2180122d2a651b423902fc773243da086c
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-16 10:36:41 -0500

    Add `NetworkBuffer.Pool`

--- Sources/Zephyr/NetworkBuffer.swift
+++ Sources/Zephyr/NetworkBuffer.swift
@@ -7,34 +7,82 @@
 
 import CZephyr
 
-/// Zepyr Network Buffer
 public struct NetworkBuffer: ~Copyable {
     
-    internal private(set) var data: [UInt8]
+    internal private(set) var buffer: UnsafeMutablePointer<net_buf>
     
-    internal private(set) var buffer = net_buf_simple()
-    
-    public init() {
-        self.data = []
-        net_buf_simple_reset(&buffer)
+    public var id: Int32 {
+        net_buf_id(buffer)
     }
     
-    public init<C>(_ data: C) where C: Collection, C.Element == UInt8 {
-        self.data = Array(data)
-        self.data.withContiguousStorageIfAvailable({
-            net_buf_simple_init_with_data(&buffer, .init(mutating: $0.baseAddress!), $0.count)
-        })
+    public var capacity: Int {
+        Int(net_buf_max_len(buffer))
     }
     
-    public init(_ other: borrowing NetworkBuffer) {
-        self.init()
-        other.withUnsafePointer {
-            net_buf_simple_clone($0, &self.buffer)
+    public init?(pool: borrowing Pool) {
+        guard let pointer = net_buf_alloc(pool.pool, k_timeout_t(ticks: 0)) else {
+            return nil
         }
-        self.data = other.data
+        self.init(pointer, retain: false)
     }
     
-    public func withUnsafePointer<T, E>(_ body: (UnsafePointer<net_buf_simple>) throws(E) -> (T)) rethrows -> T {
-        try Swift.withUnsafePointer(to: buffer, body)
+    public init(_ buffer: UnsafeMutablePointer<net_buf>, retain: Bool = false) {
+        self.buffer = buffer
+        if retain {
+            _ = net_buf_ref(buffer)
+        }
+    }
+    
+    public init?(_ other: borrowing NetworkBuffer) {
+        guard let buffer = other.withUnsafePointer({
+            net_buf_clone(.init(mutating: $0), .init(ticks: 0))
+        }) else { return nil }
+        self.init(buffer, retain: false)
+    }
+    
+    deinit {
+        net_buf_unref(buffer)
+    }
+    
+    public func withUnsafePointer<T, E>(_ body: (UnsafePointer<net_buf>) throws(E) -> (T)) rethrows -> T {
+        try body(UnsafePointer(buffer))
+    }
+    
+    public mutating func withUnsafeMutablePointer<T, E>(_ body: (UnsafeMutablePointer<net_buf>) throws(E) -> (T)) rethrows -> T {
+        try body(buffer)
+    }
+    
+    public mutating func append(_ newElement: UInt8) {
+        _ = net_buf_add_u8(buffer, newElement)
+    }
+    
+    public mutating func append(_ pointer: UnsafePointer<UInt8>, count: Int) {
+        net_buf_add_mem(buffer, pointer, count)
+    }
+    
+    //public mutating func append <C: Collection> (contentsOf bytes: C) where C.Element == UInt8 {
+    
+    
+}
+
+public extension NetworkBuffer {
+    
+    struct Pool: ~Copyable {
+        
+        public var pool: UnsafeMutablePointer<net_buf_pool>
+        
+        public var id: Int32 {
+            pool_id(pool)
+        }
+        
+        public init?(id: Int32) {
+            guard let pointer = net_buf_pool_get(id) else {
+                return nil
+            }
+            self.pool = pointer
+        }
     }
 }
+
+@_silgen_name("pool_id")
+func pool_id(_ pointer: UnsafePointer<net_buf_pool>) -> Int32
